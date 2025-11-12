6da261ca6ce1a6a8ad80fd6c2db1f4033b7497ac
arex7020 <alexandar.cvetkovic0@gmail.com>
2025-11-12 21:10:41 +0100

    added some stuff

--- main/include/collision_avoidance/ultrasound.h
+++ main/include/collision_avoidance/ultrasound.h
@@ -3,12 +3,16 @@
 
 #include "esp_attr.h"
 #include "driver/gpio.h"
+#include "esp_timer.h"
 
 #define ECHO_PIN 99
 #define TRIG_PIN 100
 
-void hcsr04_init(void);
-void hcsr04_trigger(void);
+void hcsr_init(void);
+void hcsr_trigger(void);
 void IRAM_ATTR echo_isr_handler(void *arg);
+
+static int64_t echo_start = 0;
+static float hcsr_distance = 0;
 
 #endif // ULTRASOUND_H
--- main/src/collision_avoidance/ultrasound.c
+++ main/src/collision_avoidance/ultrasound.c
@@ -1,1 +1,48 @@
 #include "collision_avoidance/ultrasound.h"
+
+#define SPEED_OF_SOUND 343.0f
+
+/* Interrupt handler for ECHO PIN */
+void IRAM_ATTR echo_isr_handler(void *arg) {
+
+    int64_t now = esp_timer_get_time();
+
+    if (gpio_get_level(ECHO_PIN) == 1)
+    {
+        echo_start = now;
+    }
+    else
+    {
+        int64_t pulse = now - echo_start;
+        hcsr_distance = (pulse / 2.0f) * SPEED_OF_SOUND / 1000000; 
+    }
+    
+}
+
+/* Init pins for collision avoidance sensor */
+void hcsr_init(void) {
+    gpio_config_t conf_hcsr_trig = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << TRIG_PIN
+    }; gpio_config(&conf_hcsr_trig);
+
+    gpio_config_t conf_hcsr_echo = {
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = 1ULL << ECHO_PIN
+    }; gpio_config(&conf_hcsr_echo);
+    
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(ECHO_PIN, echo_isr_handler, NULL);
+
+}
+
+/*  */
+void hcsr_trigger(void) {
+    gpio_set_level(TRIG_PIN, 1);
+    //put this in xtask cus esp_rom_delay literally pauses the whole cpu
+    esp_rom_delay_us(10);
+    gpio_set_level(TRIG_PIN, 0);
+}
+
+
+
